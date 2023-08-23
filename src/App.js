7fcd8611ca19f913e9536af798efcad4ed639fb3
ThafnhSown <sontnt204685@gmail.com>
2023-08-23 09:50:22 +0700

    set log

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import {useState} from 'react'
 import './App.css';
 import Cookies from 'universal-cookie';
 import axios from 'axios'
 import { COOKIE_OPTIONS } from './constant/index';
 const cookies = new Cookies()
 
+
 function App() {
+  const [log, setLog] = useState('')
   const handleLogIn = async (req, res, next) => {
     const username = 'sonidabezt5'
     const password = 'sonidabezt'
@@ -14,6 +17,9 @@ function App() {
     return await axios.post("http://localhost:3001/user/login", {username, password, deviceId})
     .then((response) => {
       console.log(response)
+      if(response.status === 200) {
+        setLog(`Hello ${username}`)
+      }
       
       cookies.set('access_token', response?.data?.accessToken, COOKIE_OPTIONS)
       cookies.set('refresh_token', response?.data?.refreshToken, COOKIE_OPTIONS)
@@ -26,6 +32,7 @@ function App() {
     await axios.post("http://localhost:3001/user/logout", {refreshToken})
     .then((response) => {
       if(response.data.status === 200) {
+        setLog('You are logged out')
         cookies.remove('access_token')
         cookies.remove('refresh_token')
       }
@@ -40,6 +47,11 @@ function App() {
       }
     }).then((response) => {
       console.log(response)
+      if(response.status === 200) {
+        const username1 = response.data[0].username
+        const username2 = response.data[1].username
+        setLog(`${username1} and ${username2}`)
+      }
     }).catch(err => {
       console.log(err)
     })
@@ -53,7 +65,9 @@ function App() {
       console.log(response)
       cookies.set("access_token", response.data.accessToken, COOKIE_OPTIONS)
       cookies.set("refresh_token", response.data.refreshToken, COOKIE_OPTIONS)
-    })
+    }).catch(err => {
+      console.log(err)
+    }) 
   }
 
 
@@ -63,6 +77,7 @@ function App() {
       <button onClick={handleLogout}>Logout</button>
       <button onClick={handleRefresh}>Refresh</button>
       <button onClick={handleList}>List</button>
+      <div>{log}</div>
     </div>
   );
 }
