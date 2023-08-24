4a02fc696ba6ae1a112476b954d262e56ff384b3
ThafnhSown <sontnt204685@gmail.com>
2023-08-24 12:09:32 +0700

    add cookie from server

--- src/App.js
+++ src/App.js
@@ -1,75 +1,66 @@
 import {useState} from 'react'
 import './App.css';
-import Cookies from 'universal-cookie';
 import axios from 'axios'
-import { COOKIE_OPTIONS } from './constant/index';
+import Cookies from 'universal-cookie'
 const cookies = new Cookies()
 
 
 function App() {
   const [log, setLog] = useState('')
+
   const handleLogIn = async (req, res, next) => {
     const username = 'sonidabezt5'
     const password = 'sonidabezt'
-    const deviceId = '1'
+    const deviceId = '13'
 
-
-    return await axios.post("http://localhost:3001/user/login", {username, password, deviceId})
+    return await axios.post("http://localhost:3001/user/login", {username, password, deviceId}, {
+      withCredentials: true
+    })
     .then((response) => {
       console.log(response)
       if(response.status === 200) {
         setLog(`Hello ${username}`)
-      }
-      
-      cookies.set('access_token', response?.data?.accessToken, COOKIE_OPTIONS)
-      cookies.set('refresh_token', response?.data?.refreshToken, COOKIE_OPTIONS)
-      
-    })
-    
+      } 
+    }) 
   }
+
   const handleLogout = async (req, res, next) => {
-    const refreshToken = cookies.get('refresh_token')
-    await axios.post("http://localhost:3001/user/logout", {refreshToken})
+    await axios.post("http://localhost:3001/user/logout",{},  {
+      withCredentials:true
+    })
     .then((response) => {
       if(response.data.status === 200) {
         setLog('You are logged out')
-        cookies.remove('access_token')
-        cookies.remove('refresh_token')
       }
+    }).catch((err) => {
+      console.log(err)
     })
-    
   }
 
   const handleList = async (req, res, next) => {
     return await axios.get("http://localhost:3001/user/list", {
       headers: {
-        Authorization : 'Bearer ' + cookies.get('access_token')
+        Authorization: 'Bearer ' + cookies.get('access_token')
       }
-    }).then((response) => {
+    })
+    .then((response) => {
       console.log(response)
-      if(response.status === 200) {
-        const username1 = response.data[0].username
-        const username2 = response.data[1].username
-        setLog(`${username1} and ${username2}`)
-      }
     }).catch(err => {
       console.log(err)
     })
   }
   const handleRefresh = async ( req, res, next ) => {
-    const refreshToken = cookies.get("refresh_token")
-    const deviceId = "1"
-    return await axios.post("http://localhost:3001/user/refresh", {
-      refreshToken, deviceId
+    const deviceId = "41"
+    return await axios.get("http://localhost:3001/user/refresh", {
+      withCredentials: true
+    }, {
+     deviceId
     }).then(response => {
       console.log(response)
-      cookies.set("access_token", response.data.accessToken, COOKIE_OPTIONS)
-      cookies.set("refresh_token", response.data.refreshToken, COOKIE_OPTIONS)
     }).catch(err => {
       console.log(err)
     }) 
   }
-
 
   return (
     <div>
